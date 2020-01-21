edea5d82b07edcfae42ea48f861d4190572c9d1a
andrew leu <andrewwforsale@gmail.com>
2020-01-20 18:18:51 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Andrew Leu on 1/20/20.
+//  Copyright © 2020 Andrew Leu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
